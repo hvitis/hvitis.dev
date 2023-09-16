6e61c8a43de279dea351c4f2803debf97c4079c7
Adam Piskorek <adam.piskorek.pl@gmail.com>
2023-09-16 14:12:26 +0200

    script-src for gtag

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,15 +1,15 @@
 const projectsData = [
   {
-    title: 'Book cataloguing system',
-    description: `Catalogue your paper book collection with printed QR codes. Search other bookshelves and become a librarian in your own neighbourhood.`,
-    imgSrc: '/static/images/assets/youtalky.png',
-    href: 'https://www.youtalky.com',
-  },
-  {
     title: 'Mosaic Art Maker',
     description: `Change images to LEGO mosaics. Reuse old LEGO sets that you have and get order and building instructions. Get tiles lists for other programs.`,
     imgSrc: '/static/images/assets/mosaic-art-maker.png',
     href: '/mosaic-art-maker',
+  },
+  {
+    title: 'Book cataloguing system',
+    description: `Catalogue your paper book collection with printed QR codes. Search other bookshelves and become a librarian in your own neighbourhood.`,
+    imgSrc: '/static/images/assets/youtalky.png',
+    href: 'https://www.youtalky.com',
   },
 ]
 

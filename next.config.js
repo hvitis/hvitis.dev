3fad73a013315eaebf424df3b5ef91d53aec8e13
Adam Piskorek <adam.piskorek.pl@gmail.com>
2023-09-16 14:19:03 +0200

    script-src update 🔏

--- next.config.js
+++ next.config.js
@@ -7,8 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is *.googletagmanager.com;
-  script-src 'self' *.googletagmanager.com *.google-analytics.com;
+  script-src 'self' *.googletagmanager.com *.google-analytics.com analytics.umami.is giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;

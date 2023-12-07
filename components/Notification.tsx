9c70c5746efd3ea5332fd881883c25e0bcc3c057
Adam Piskorek <adam.piskorek.pl@gmail.com>
2023-12-07 03:19:01 +0100

    ids

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -23,17 +23,23 @@ const Notification = ({ title, msg, btn, href, className }: NotificationInterfac
         <div className={clsx(className, 'alert shadow-lg my-3')}>
           <InfoIcon className="stroke-info shrink-0 w-6 h-6" />
           <div>
-            <h3 className="font-bold">{title}</h3>
+            <h3 id="notification-title" className="font-bold">
+              {title}
+            </h3>
             <div className="text-sm" dangerouslySetInnerHTML={{ __html: msg }}></div>
           </div>
           <div className="flex flex-row">
             {href && (
-              <Link href={href} className="btn btn-sm text-blue-600">
+              <Link id="notification-link" href={href} className="btn btn-sm text-blue-600">
                 {btn || 'Read more'}
               </Link>
             )}
             <div className="tooltip" data-tip={"Don't show again"}>
-              <button id="notification" className="btn btn-sm" onClick={() => setVisible(false)}>
+              <button
+                id="notification-close"
+                className="btn btn-sm"
+                onClick={() => setVisible(false)}
+              >
                 <XCircleIcon className="text-gray-500" />
               </button>
             </div>

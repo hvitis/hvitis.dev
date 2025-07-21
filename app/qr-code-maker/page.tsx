7060c1265e6f1456d698e5825ae9f44180899ad0
Adam Piskorek <adam.piskorek.pl@gmail.com>
2025-07-21 13:36:32 +0200

    fix build

--- app/qr-code-maker/page.tsx
+++ app/qr-code-maker/page.tsx
@@ -3,7 +3,7 @@ import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({
   title: 'QR Code Maker',
-  description: 'Create your own QR codes for free. Just enter a link and download the QR code as a PNG or SVG file.',
+  description: 'Create your own QR codes for free. ',
 })
 
 export default function Page() {
